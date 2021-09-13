9bdf699eacf295f46bc71a546583cf3f8323ae20
Alexandre Oliveira <alexandre.si.ufu@gmail.com>
2021-09-13 20:35:12 -0300

    RestTemplate

---
+++ hr-payroll/src/main/java/com/iftm/ms/hrpayroll/entities/Worker.java
@@ -0,0 +1,18 @@
+package com.iftm.ms.hrpayroll.entities;
+
+import java.io.Serializable;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class Worker implements Serializable {
+	private static final long serialVersionUID = 1L;
+	
+	private Long id;
+	private String name;
+	private Double dailyIncome;	
+}
