6efcaac5ec71d28c4c7d3fb2bf27dec90b4420d5
Alexandre Oliveira <alexandre.si.ufu@gmail.com>
2021-09-27 19:39:40 -0300

    Project hr-eureka-server created

---
+++ hr-eureka-server/src/test/java/com/iftm/ms/hreurekaserver/HrEurekaServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.iftm.ms.hreurekaserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HrEurekaServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
