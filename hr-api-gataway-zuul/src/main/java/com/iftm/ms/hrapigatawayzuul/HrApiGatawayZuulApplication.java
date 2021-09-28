fafcd4e4d1a41d228a3d9f5ab3682bdf373d07d7
Alexandre Oliveira <alexandre.si.ufu@gmail.com>
2021-09-27 21:43:55 -0300

    Project hr-api-gateway-zuul created

---
+++ hr-api-gataway-zuul/src/main/java/com/iftm/ms/hrapigatawayzuul/HrApiGatawayZuulApplication.java
@@ -0,0 +1,13 @@
+package com.iftm.ms.hrapigatawayzuul;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HrApiGatawayZuulApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HrApiGatawayZuulApplication.class, args);
+	}
+
+}
